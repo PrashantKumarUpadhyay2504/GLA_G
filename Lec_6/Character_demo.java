6e10563fcac5f49de2bf536f3ff925ec7413b827
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-06-27 07:50:15 +0530

    Datatypes and Functions

---
+++ Lec_6/Character_demo.java
@@ -0,0 +1,14 @@
+package Lec_9;
+
+public class Character_demo {
+    public static void main(String[] args) {
+        char b='A';
+        int c=(int)b;
+//        System.out.println(c);
+        char d=(char)(b+1);
+//        System.out.println(d);
+        char e='u'+'9';
+        System.out.println(e);
+        System.out.println((int)e);
+    }
+}
---
+++ Lec_6/Functions.java
@@ -0,0 +1,22 @@
+package Lec_6;
+
+public class Functions {
+    public static void main(String[] args) {
+
+        add(1,2);
+        print();
+    }
+
+    public  static void print(){
+
+        System.out.println("Hello World");
+    }
+    public static void add(int a,int b){
+        sub(5,1);
+        System.out.println(a+b);
+    }
+    public static void sub(int a,int b){
+        System.out.println(a-b);
+    }
+
+}
